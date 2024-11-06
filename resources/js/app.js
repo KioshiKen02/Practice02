af2c161546e3ee0d45aaa357ba38864cf094c599
KIOSHI <kioshi.ken02@gmail.com>
2024-11-06 10:40:39 +0800

    Initials

---
+++ resources/js/app.js
@@ -0,0 +1,12 @@
+import './bootstrap';
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
+
+import { createApp } from 'vue';
+import App from './components/app.vue';
+
+createApp(App).mount('#app');
---
+++ resources/js/components/app.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <h1>Hello, Vue!</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'App'
+  }
+  </script>
+  
+  <style scoped>
+  /* Your component styles here */
+  </style>
+  
